bc3b0c95ec817e683ac72979e2e5cfe59939879c
Haider Yaqoob <49457040+haideryaqoob720@users.noreply.github.com>
2023-04-29 19:33:59 +1000

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -2,6 +2,8 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 const fs = require("fs");
 const accountPrivatekey = fs.readFileSync(".secret").toString().trim();
+const infuraMainKey =
+  "wss://mainnet.infura.io/ws/v3/f5846a03eaec47c28b102d6d01a2de16";
 
 module.exports = {
   networks: {
@@ -9,6 +11,17 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*", // Match any network id
+    },
+    main: {
+      networkCheckTimeout: 1000000,
+      provider: () => new HDWalletProvider(accountPrivatekey, infuraMainKey),
+      // provider: () => new HDWalletProvider(mnemonic, infuraMainKey),
+
+      network_id: 1, // Ropsten's id
+      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 5, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
     bsctestnet: {
       networkCheckTimeout: 1500000,
